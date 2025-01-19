bf49837392a188da50138ce47d1e4efe9a40c511
rakib8680 <rakib.cst.1st@gmail.com>
2025-01-19 19:49:25 +0600

    Add bcrypt password hashing to encrypt pass

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,4 +7,5 @@ dotenv.config({path: path.join(process.cwd(), '.env')});
 export default{
     port : process.env.PORT || 5000,
     databaseURL : process.env.DATABASE_URL,
+    bcryptSalt : process.env.BCRYPT_SALT_ROUNDS,
 }
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,38 +1,45 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
 import { userRoles } from "./user.constant";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
-
-
-const userSchema = new Schema<TUser>({
-    username:{
-        type:String,
-        unique:true,
-        trim:true,
-        required:[true,"Username is required"]
+const userSchema = new Schema<TUser>(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: [true, "Username is required"],
     },
-    email:{
-        type:String,
-        unique:true,
-        required:[true,"Email is required"]
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
     },
-    password:{
-        type:String,
-        required:[true,"Password is required"]
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    role:{
-        type:String,
-        enum:{
-            values:userRoles,
-            message:`{VALUE} is not a valid role`
-        },
-        default:"user"
-    }
-},{
-    timestamps:true
-})
+    role: {
+      type: String,
+      enum: {
+        values: userRoles,
+        message: `{VALUE} is not a valid role`,
+      },
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-
+// hash the password before saving to DB
+userSchema.pre("save", async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(user?.password, Number(config.bcryptSalt));
+  next();
+});
 
 export const UserModel = model<TUser>("user", userSchema);
