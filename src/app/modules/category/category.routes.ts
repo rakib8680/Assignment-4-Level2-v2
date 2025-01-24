4c3c51402f9941f15b6298871f061b7cb1d2c11b
rakib8680 <rakib.cst.1st@gmail.com>
2025-01-24 19:14:03 +0600

    Add authentication middleware and user role constant to category routes

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { CategoryController } from "./category.controller";
 import requestValidation from "../../middlewares/requestValidation";
 import { categoryValidationSchema } from "./category.validation";
+import auth from "../../middlewares/auth";
+import { USER_ROLE_ENUM } from "../user/user.constant";
 
 const router = Router();
 
 router.get("/all-categories", CategoryController.getAllCategories);
 router.post(
   "/create-category",
+  auth(USER_ROLE_ENUM.admin),
   requestValidation(categoryValidationSchema),
   CategoryController.createCategory
 );
