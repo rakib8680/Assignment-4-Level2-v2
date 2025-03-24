ee0c7bcb1ba5cabd363eae2ebe04ee0499d798df
rakib8680 <rakib.cst.1st@gmail.com>
2025-03-24 17:53:39 +0600

    Update toObject method in user.interface.ts

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -3,7 +3,7 @@ import { Model } from "mongoose";
 export type TUserRole = "admin" | "user";
 
 export interface TUser {
-  toObject(): unknown;
+  toObject(): unknown; // toObject method is used to make your mongoose document object to plain object so that you can delete any field after creating the document. You can also use toJSON method to achieve the same. Also without this you can actually delete any field from the document object.
   _id?: string;
   username: string;
   email: string;
